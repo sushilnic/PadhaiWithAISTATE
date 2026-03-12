4792dc0dcc744473e8a0d24106bfce88344c2149
sushilnic <65997461+sushilnic@users.noreply.github.com>
2026-03-12 16:33:05 +0530

    Update wsgi.py

--- school_project/school_project/wsgi.py
+++ school_project/school_project/wsgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+# Fix path for Appliku deployment
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_project.settings')
