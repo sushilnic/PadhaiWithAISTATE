4220609b0fb04fb35404a5af05eae04f3412a6f6
sushilnic <65997461+sushilnic@users.noreply.github.com>
2026-03-13 12:25:05 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -e
 cd school_project
 export PYTHONPATH=/code/school_project
-gunicorn school_project.wsgi:application --log-file - --bind 0.0.0.0:8001
+gunicorn school_project.wsgi:application --log-file - --bind 0.0.0.0:8000
